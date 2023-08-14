13e2af9e8c00eb35e24056cde07dc610b843f174
BellXP <u3008401@connect.hku.hk>
2023-08-14 08:09:25 +0800

    try update

--- try.py
+++ try.py
@@ -20,32 +20,42 @@ def eval_yes_no(data_path):
         predictions = json.load(f)
 
     eval = VQAEval()
-    correct = 0
-    no_gts = 0
-    num_yes = 0
-    num_no = 0
     num = 0
+    no_gts = 0
+    wrong_yes, correct_yes = 0, 0
+    wrong_no, correct_no = 0, 0
     for i in range(len(predictions)):
         gt_answers = predictions[i]['gt_answers']
         answer = predictions[i]['answer']
-        if eval.evaluate(answer, gt_answers) == 1:
-            correct += 1
         has_yes = eval.evaluate(answer, 'yes') == 1
         has_no = eval.evaluate(answer, 'no') == 1
+        
         if has_yes:
-            num_yes += 1
+            if gt_answers == 'yes':
+                correct_yes += 1
+            else:
+                wrong_yes += 1
         if has_no:
-            num_no += 1
+            if gt_answers == 'no':
+                correct_no += 1
+            else:
+                wrong_no += 1
         if not has_yes and not has_no:
             no_gts += 1
-
         num+=1
 
+    num_no = correct_no + wrong_no
+    num_yes = correct_yes + wrong_yes
+    correct = correct_yes + correct_no
     print(f'Total num: {num}; Correct: {correct}; No any gts: {no_gts}')
     print(f'Accuracy: {float(correct)/num}')
     print(f'Ratio of no gts: {float(no_gts)/num}')
     print(f'Ratio of yes: {float(num_yes)/num}')
     print(f'Ratio of no: {float(num_no)/num}')
+    print(f"Ratio of correct yes: {float(correct_yes)/num}")
+    print(f"Ratio of correct no: {float(correct_no)/num}")
+    print(f"Ratio of wrong yes: {float(wrong_yes)/num}")
+    print(f"Ratio of wrong no: {float(wrong_no)/num}")
 
 
 for suffix in ['', '_Yes', '_No']:
